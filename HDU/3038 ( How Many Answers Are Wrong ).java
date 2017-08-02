cb079c517ef94ae125ab6ae7fa09f7e54da35d96
linxiaoby <wanglin_bnu@163.com>
2017-08-02 15:43:27 +0800

    POJ 1182

---
+++ HDU/3038 ( How Many Answers Are Wrong ).java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static final int MAXN = 200000 + 3;
+	static int[] p = new int[MAXN], s = new int[MAXN];
+	
+	static int find(int x) {
+		if(p[x] == -1) return x;
+		int k = p[x];
+		p[x] = find(p[x]);
+		s[x] += s[k];
+		return p[x];
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt(), M = in.nextInt();
+		Arrays.fill(p, 0, N + 1, -1); 
+		Arrays.fill(s, 0, N + 1, 0);
+		
+		int a, b, v, ans = 0;
+		while(M-- > 0) {
+			a = in.nextInt(); b = in.nextInt(); v = in.nextInt();
+			a--;
+			int x = find(a), y = find(b);
+			if(x != y) {   
+				p[y] = x;
+				s[y] = s[a] - s[b] + v;
+			}
+			else if(s[b] != s[a] + v) 
+				ans++;
+		}
+		System.out.println(ans);
+	}
+}
--- POJ/1182 ( 食物链 ).java
+++ POJ/1182 ( 食物链 ).java
@@ -1,52 +1,7 @@
-【基本并查集】
-static int find(int x) {
-		return p[x] == x ? x : (p[x] = find(p[x]));
-	}
-
-static void union(int x, int y) {
-	int a = find(x), b = find(y);
-	if(a != b)
-		p[a] = b;
-}
-=======================================
-【基本并查集例题：HDU1213】
-import java.util.Scanner;
-
-public class Main {
-	static final int MAXN = 1000 + 7;
-	static int[] p = new int[MAXN];
-	
-	static int find(int x) {
-		return p[x] == x ? x : (p[x] = find(p[x]));
-	}
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int T = in.nextInt();
-		int N, M;
-		while(T-- > 0){
-			N = in.nextInt(); M = in.nextInt();
-			for(int i = 1; i <= N; i++)
-				p[i] = i;
-			
-			int ans = N;
-			for(int i = 0; i < M; i++) {
-				int a = find(in.nextInt()), b = find(in.nextInt());
-				if(a != b) {
-					p[a] = b;
-					ans--;
-				}
-			}
-			System.out.println(ans);
-		}
-	}
-}
-===============================================
-【带权并查集例题：POJ 1182 ( 食物链 )】
-
 /**
- * 0:和根节点同类; 1:被根节点吃; 2:吃根节点
- * 在合并时节点权值的更新方式要和权值定义保持一致。
+ * (1)0:和根节点同类; 1:被根节点吃; 2:吃根节点
+ * 		在合并时节点权值的更新方式要和权值定义保持一致。
+ * (2)scanner会TLE
  */
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -92,4 +47,3 @@ public class Main {
 		System.out.println(ans);
 	}
 }
-
