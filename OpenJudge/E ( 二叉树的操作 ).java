4a59fac939e8d2d9f0f179ab23da97260d98a903
linxiaoby <wanglin_bnu@163.com>
2017-09-12 22:01:12 +0800

    2201

--- OpenJudge/E ( 二叉树的操作 ).java
+++ OpenJudge/E ( 二叉树的操作 ).java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
 	static class Node {
 		int l, r, p;//左孩子、右孩子以及父亲节点的下标
-		char tag;//标志该节点是父亲节点的左孩子还是右孩子 'l', 'r'，便于节点交换时，修改双方父亲对应的孩子
 	}
 	
 	static final int MAXN = 100 + 3;
 	static Node[] no = new Node[MAXN];
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		for(int i = 0; i < MAXN; i++) 
@@ -20,14 +20,10 @@ public class Main {
 			for(int i = 0; i < n; i++) {
 				x = in.nextInt(); y = in.nextInt(); z = in.nextInt();
 				no[x].l = y; no[x].r = z;
-				if(y != -1) {
+				if(y != -1) 
 					no[y].p = x; 
-					no[y].tag = 'l';	
-				}
-				if(z != -1) {
+				if(z != -1) 
 					no[z].p = x;
-					no[z].tag = 'r';
-				} 
 			}
 			no[0].p = -1;
 			
@@ -38,10 +34,14 @@ public class Main {
 					//修改双方父亲的孩子
 					x = in.nextInt(); y = in.nextInt();
 					int px = no[x].p; int py = no[y].p;
-					if(no[x].tag == 'l') no[px].l = y;
-					else if(no[x].tag == 'r') no[px].r = y;
-					if(no[y].tag == 'l') no[py].l = x;
-					else if(no[y].tag == 'r') no[py].r = x;
+					char tag1 = 'l', tag2 = 'l';
+					if(no[px].r == x) tag1 = 'r';
+					if(no[py].r == y) tag2 = 'r';
+					
+					if(tag1 == 'l') no[px].l = y;
+					else no[px].r = y;
+					if(tag2 == 'l') no[py].l = x;
+					else no[py].r = x;
 					
 					//交换双方的父亲，
 					no[x].p = py;
@@ -58,3 +58,4 @@ public class Main {
 		}
 	}
 }
+
