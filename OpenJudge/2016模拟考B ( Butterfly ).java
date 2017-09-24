9c5b0cf29213dea1815f5d8ab36ecdc119b4990a
linxiaoby <wanglin_bnu@163.com>
2017-09-24 20:40:03 +0800

    2039

--- OpenJudge/2016模拟考B ( Butterfly ).java
+++ OpenJudge/2016模拟考B ( Butterfly ).java
@@ -1,38 +1,44 @@
-package Hdu;
-
+/**
+ * 同在一棵树的节点是关系已经确定的节点
+ */
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static final int MAXN = 1000 + 3;
-	static int[] p = new int[MAXN];
+	static final int MAXN = 1000 + 3, MAXM = 100000 + 3;
+	static int[] p = new int[MAXN], d = new int[MAXN];
 	
 	static int find(int x) {
-		return p[x] == x ? x : (p[x] = find(p[x]));
+		if(p[x] == -1) 
+			return x;
+		int tmp = find(p[x]);
+		d[x] = (d[x] + d[p[x]]) % 2;
+		return p[x] = tmp;
 	}
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n, m;
-		while(true) {
+		while(in.hasNext()) {
 			n = in.nextInt(); m = in.nextInt();
-			for(int i = 0; i < n; i++) 
-				p[i] = i;
-			int x, y, r, a, b;
+			Arrays.fill(p, 0, n + 7, -1); 
+			Arrays.fill(d, 0, n + 7, 0);
 			boolean ok = true;
 			for(int i = 0; i < m; i++) {
-				x = in.nextInt(); 
-				y = in.nextInt();
-				r = in.nextInt();
-				a = find(x);
-				b = find(y);
-				if((r == 1 && a == b) || (r == 0 && a != b ) ) {
-					ok = false; break;
+				int a = in.nextInt(), b = in.nextInt(), r = in.nextInt();
+				int x = find(a), y = find(b);
+				if(x == y) {//如果a和b此前已经确定关系
+					if((r == 0 && d[a] != d[b]) || (r == 1 && d[a] == d[b])) {
+						ok = false;
+						break;
+					}
 				}
-				else if(r == 0 && )
+				else {//a和b此前没有确定关系，则将a和b所在的子树合并
+					p[x] = y;
+					d[x] = ((2 - d[a]) + r + d[b]) % 2;
+				}
 			}
+			System.out.println(ok ? "YES" : "NO");
 		}
 	}
 }
-
-
--- POJ/1182 ( 食物链 ).java
+++ POJ/1182 ( 食物链 ).java
@@ -1,6 +1,7 @@
 /**
- * (1)0:和根节点同类; 1:被根节点吃; 2:吃根节点
- * 		在合并时节点权值的更新方式要和权值定义保持一致。
+ * (1)0:和根节点同类; 1:吃根节点; 2:被根节点吃
+ * (2)d[i]:节点i指向根节点的关系，若有关系 r x y，rootx = a，rooty = b,
+ *		则合并时应当让p[a] = b。若d[i]表示根节点指向节点i的关系，那么p[b] = a
  * (2)scanner会TLE
  */
 import java.io.BufferedReader;
@@ -10,39 +11,37 @@ import java.util.Arrays;
 
 public class Main {
 	static final int MAXN = 50000 + 3;
-	static int[] p = new int[MAXN], r = new int[MAXN];
+	static int[] p = new int[MAXN], d = new int[MAXN];
 	
 	static int find(int x) {
-		if (p[x] == -1) return x;
-		int k = p[x];
-		p[x] = find(p[x]);
-		r[x] = (r[x] + r[k]) % 3;
-		return p[x];
+		if(p[x] == -1) return x;
+		int tmp = find(p[x]);
+		d[x] = (d[x] + d[p[x]]) % 3;
+		return p[x] = tmp;
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] str = br.readLine().split(" ");
-		int N = Integer.valueOf(str[0]), K = Integer.valueOf(str[1]);
-		
+		String[] tmp = br.readLine().split(" ");
+		int n = Integer.valueOf(tmp[0]), k = Integer.valueOf(tmp[1]), r, x, y;
 		Arrays.fill(p, -1);
-		int d, x, y, ans = 0;
-		while(K-- > 0) {
-			str = br.readLine().split(" ");
-			d = Integer.valueOf(str[0]); 
-			x = Integer.valueOf(str[1]); y = Integer.valueOf(str[2]);
-			
-			if(x > N || y > N || (d == 2 && x == y)) {
-				ans++; 
+		Arrays.fill(d, 0);
+		
+		int ans = 0;
+		for(int i = 0; i < k; i++) {
+			tmp = br.readLine().split(" ");
+			r = Integer.valueOf(tmp[0]); x = Integer.valueOf(tmp[1]); y = Integer.valueOf(tmp[2]);
+			if(x > n || y > n || (r == 2 && x == y)) {
+				ans ++ ;
 				continue;
 			}
 			int a = find(x), b = find(y);
-			if(a != b) {
-				p[b] = a; 
-				r[a] = (3 - r[x] + r[y] + d - 1) % 3;
+			if((a == b) && d[x] != (d[y] + r - 1) % 3) 
+					ans++;
+			else if(a != b){	
+				p[a] = b;
+				d[a] = (3 - d[x] + r - 1 + d[y]) % 3;
 			}
-			else if((d == 1 && r[x] != r[y]) || (d == 2 && r[x] != (r[y] + 1) % 3))
-				ans++; 	 	
 		}
 		System.out.println(ans);
 	}
